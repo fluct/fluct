d1f612a4a3d16fd085c3e9b8cc733c8792dc110b
Ryo Nakamura <r7kamura@gmail.com>
2015-08-05 02:28:12 +0900

    Add #deleteDefaultModel and #createRestapi

--- src/synthesizer.js
+++ src/synthesizer.js
@@ -1,3 +1,4 @@
+import { Client } from 'amazon-api-gateway-client'
 import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
@@ -8,41 +9,100 @@ import yaml from 'js-yaml'
 export default class Synthesizer {
   /**
    * @param {String} accessKeyId
+   * @param {Client=} client
    * @param {String} region
    * @param {String} secretAcceessKey
    * @param {String} swaggerFilePath
    */
-  constructor({ accessKeyId, region, secretAcceessKey, swaggerFilePath }) {
+  constructor({ accessKeyId, client, region, secretAcceessKey, swaggerFilePath }) {
     this.accessKeyId = accessKeyId;
+    this._client = client;
     this.region = region;
     this.secretAcceessKey = secretAcceessKey;
     this.swaggerFilePath = swaggerFilePath;
   }
 
   /**
+   * @return {Promise}
+   */
+  createRestapi() {
+    return this.getClient().createRestapi({
+      name: this.getSwagger().info.title
+    });
+  }
+
+  /**
+   * @param {String} restapiId
+   * @return {Promise}
+   */
+  deleteDefaultModels({ restapiId }) {
+    return Promise.all(
+      [
+        'Empty',
+        'Error'
+      ].map((modelName) => {
+        return this.getClient().deleteModel({
+          modelName: modelName,
+          restapiId: restapiId
+        });
+      })
+    );
+  }
+
+  /**
+   * @return {Client}
+   */
+  getClient() {
+    if (!this._client) {
+      this._client = new Client({
+        accessKeyId: this.accessKeyId,
+        region: this.region,
+        secretAcceessKey: this.secretAcceessKey
+      });
+    }
+    return this._client;
+  }
+
+  /**
    * @return {Array.<String>}
    */
   getPaths() {
-    const basePath = this._getBasePath();
-    return Object.keys(this._getSwagger().paths).map((entrypointPath) => {
+    const basePath = this.getBasePath();
+    return Object.keys(this.getSwagger().paths).map((entrypointPath) => {
       return path.join(basePath, entrypointPath);
     });
   }
 
   /**
+   * @param {Function} middleware
+   * @param {Object=} options
+   * @return {Synthesizer}
+   */
+  use(middleware, options) {
+    return new this.constructor({
+      accessKeyId: this.accessKeyId,
+      client: this.getClient().use(middleware, options),
+      region: this.region,
+      secretAcceessKey: this.secretAcceessKey,
+      swaggerFilePath: this.swaggerFilePath
+    });
+  }
+
+  /**
+   * @private
    * @return {String}
    */
-  _getBasePath() {
-    return this._getSwagger().basePath || '/';
+  getBasePath() {
+    return this.getSwagger().basePath || '/';
   }
 
   /**
    * @private
    * @return {Object}
    */
-  _getSwagger() {
+  getSwagger() {
     if (!this._swagger) {
-      this._swagger = this._parseSwaggerFile();
+      this._swagger = this.parseSwaggerFile();
     }
     return this._swagger;
   }
@@ -51,7 +111,7 @@ export default class Synthesizer {
    * @private
    * @return {Object}
    */
-  _parseSwaggerFile() {
+  parseSwaggerFile() {
     return yaml.safeLoad(fs.readFileSync(this.swaggerFilePath, 'utf8'));
   }
 }
--- test/synthesizer-test.js
+++ test/synthesizer-test.js
@@ -1,9 +1,34 @@
 import assert from 'assert'
+import { Mock } from 'stackable-fetcher'
 import { Synthesizer } from '../src/index'
 
 describe('Synthesizer', () => {
   const synthesizer = new Synthesizer({
+    accessKeyId: 'accessKeyId',
+    region: 'region',
+    secretAcceessKey: 'secretAcceessKey',
     swaggerFilePath: 'examples/swagger.yml'
+  }).use(
+    Mock,
+    {}
+  );
+
+  describe('#createRestapi', () => {
+    it('sends request to create new restapi', () => {
+      assert(
+        synthesizer.createRestapi() instanceof Promise
+      );
+    });
+  });
+
+  describe('#deleteDefaultModels', () => {
+    it('sends requests to delete default models', () => {
+      assert(
+        synthesizer.deleteDefaultModels({
+          restapiId: 'restapiId'
+        }) instanceof Promise
+      );
+    });
   });
 
   describe('#getPaths', () => {
@@ -14,6 +39,14 @@ describe('Synthesizer', () => {
           '/v1/products',
           '/v1/products/child'
         ]
+      );
+    });
+  });
+
+  describe('#use', () => {
+    it('returns new Synthesizer', () => {
+      assert(
+        synthesizer.use(() => {}) instanceof Synthesizer
       );
     });
   });
