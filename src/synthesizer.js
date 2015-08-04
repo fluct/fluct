bec75bc5c21986a277875be24d68e7f4dd3a89b4
Ryo Nakamura <r7kamura@gmail.com>
2015-08-05 01:38:38 +0900

    Add Synthesizer#getPaths

--- src/synthesizer.js
+++ src/synthesizer.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import path from 'path'
+import yaml from 'js-yaml'
+
 /**
  * @class
  */
@@ -6,10 +10,48 @@ export default class Synthesizer {
    * @param {String} accessKeyId
    * @param {String} region
    * @param {String} secretAcceessKey
+   * @param {String} swaggerFilePath
    */
-  constructor({ accessKeyId, region, secretAcceessKey }) {
+  constructor({ accessKeyId, region, secretAcceessKey, swaggerFilePath }) {
     this.accessKeyId = accessKeyId;
     this.region = region;
     this.secretAcceessKey = secretAcceessKey;
+    this.swaggerFilePath = swaggerFilePath;
+  }
+
+  /**
+   * @return {Array.<String>}
+   */
+  getPaths() {
+    const basePath = this._getBasePath();
+    return Object.keys(this._getSwagger().paths).map((entrypointPath) => {
+      return path.join(basePath, entrypointPath);
+    });
+  }
+
+  /**
+   * @return {String}
+   */
+  _getBasePath() {
+    return this._getSwagger().basePath || '';
+  }
+
+  /**
+   * @private
+   * @return {Object}
+   */
+  _getSwagger() {
+    if (!this._swagger) {
+      this._swagger = this._parseSwaggerFile();
+    }
+    return this._swagger;
+  }
+
+  /**
+   * @private
+   * @return {Object}
+   */
+  _parseSwaggerFile() {
+    return yaml.safeLoad(fs.readFileSync(this.swaggerFilePath, 'utf8'));
   }
 }
--- test/synthesizer-test.js
+++ test/synthesizer-test.js
@@ -1,9 +1,20 @@
+import assert from 'assert'
 import { Synthesizer } from '../src/index'
 
 describe('Synthesizer', () => {
-  describe('#constructor', () => {
-    it('takes an Object', () => {
-      new Synthesizer({});
+  const synthesizer = new Synthesizer({
+    swaggerFilePath: 'examples/swagger.yml'
+  });
+
+  describe('#getPaths', () => {
+    it('returns all paths defined in swagger file', () => {
+      assert.deepEqual(
+        synthesizer.getPaths(),
+        [
+          '/v1/products',
+          '/v1/products/child'
+        ]
+      );
     });
   });
 });
