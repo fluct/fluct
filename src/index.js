affeb7a765a01cb9cd11b381223a4b76e7949432
Ryo Nakamura <r7kamura@gmail.com>
2015-08-12 01:45:31 +0900

    Remove unnecessary export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Composer from './composer'
-import GenerateCommand from './generate_command'
 
 export default {
-  Composer: Composer,
-  GenerateCommand: GenerateCommand
+  Composer: Composer
 }
