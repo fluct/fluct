f61c4d1282cc8fc8f9c4e5fffbc988512431ff37
Seraphicer <tomoyuki.kakuda@gmail.com>
2015-08-25 16:09:14 +0900

    [add] Bring node-config into fluct

--- src/composer.js
+++ src/composer.js
@@ -122,6 +122,11 @@ export default class Composer extends EventEmitter {
           zipFile.addFile(path, path);
         }
       });
+      glob.sync(`./config/*`).forEach((path) => {
+        if (!fs.lstatSync(path).isDirectory()) {
+          zipFile.addFile(path, path);
+        }
+      });
       zipFile.outputStream.pipe(
         fs.createWriteStream(zipPath)
       ).on('close', () => {
